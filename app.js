ec83cf72913cc8a1e695733939ea0e7a7d27b524
Vito Medved <vito.medved12@gmail.com>
2017-07-09 02:16:30 +0000

    Add functionality to add campground to database, add description variable to campgrounds

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.get("/", function(req, res){
 //SCHEMA setup ---------------------------------------------------
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -25,8 +26,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //     {
 //         name: "Green Mystic",
-//         image: "https://source.unsplash.com/mzZVGFfMOkA"
-        
+//         image: "https://source.unsplash.com/mzZVGFfMOkA",
+//         description: "This is a huge nature-side camp, enjoy!"
 //     }, function(err, campground){
 //         if(err){
 //             console.log("Error " + err);
@@ -35,23 +36,24 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //         }
     
 // });
-
+//INDEX ROUTE - SHOW ALL CAMPGROUNDS
 app.get("/campgrounds", function(req, res){
     //get all campgrounds from db
     Campground.find({}, function(err, camps){
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds", {campgrounds: camps});
+            res.render("index", {campgrounds: camps});
         }
     });
 });
-
+//CREATE ROUTE - ADD CAMP TO DATABASE
 app.post("/campgrounds", function(req, res){
     //gett data from form and add to campgrounds array
-    var name = req.body.name;
-    var img = req.body.image;
-    var newCampground = {name: name, image: img};
+    var name = req.body.name,
+        img = req.body.image,
+        description = req.body.description;
+    var newCampground = {name: name, image: img, description: description};
     //create new camp and save to db
     Campground.create(newCampground, function(err, camp){
         if(err){
@@ -63,9 +65,24 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+//NEW ROUTE - SHOW FORM TO CREATE CAMPGROUND
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
 });
+
+//SHOW ROUTE - SHOWS INFO ABOUT CAMPGROUND
+app.get("/campgrounds/:id", function(req, res) {
+    //findcamp with id
+    Campground.findById(req.params.id, function(err, camps){
+        if(err){
+            console.log("ERROR \n" + err + "\n ERROR");
+        }else{
+            //console.log(camps + "\n");
+            res.render("show", {campground: camps});
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Starting server...");
